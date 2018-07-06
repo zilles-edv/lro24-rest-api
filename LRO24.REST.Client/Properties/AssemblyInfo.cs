7d053b030692931337765fe942fbe3365b68461f
Jonas Zilles <zillesj-ext@idemtelematics.com>
2018-07-06 06:33:33 +0200

    1.8.2

--- LRO24.REST.Client/Properties/AssemblyInfo.cs
+++ LRO24.REST.Client/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("LRO24.REST.Contract")]
+[assembly: AssemblyTitle("LRO24.REST.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LRO24.REST.Contract")]
-[assembly: AssemblyCopyright("Copyright © Jonas Zilles: EDV-Dienstleistungen 2018")]
+[assembly: AssemblyProduct("LRO24.REST.Client")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("e72bd453-ee82-4ac3-a19a-3d1b0b050e15")]
+[assembly: Guid("e2e598d1-95b2-4fa3-9c68-8363926c281b")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.2.0")]
-[assembly: AssemblyFileVersion("1.8.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
